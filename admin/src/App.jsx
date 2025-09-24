55f4661dba1c5702620ecc2236811ab44daf2cda
1lelanta <kekikoki4@gmail.com>
2025-09-24 12:08:36 +0300

    add css propert for navbar

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Navbar from './Components/NavBar/NavBar';
 
 const App = () => {
   return (
--- admin/src/Components/NavBar/Navbar.jsx
+++ admin/src/Components/NavBar/Navbar.jsx
@@ -6,8 +6,8 @@ import navProfile from '../../assets/nav-profile.svg'
 const Navbar = () => {
   return (
     <div className='navbar'>
-      <img src="navlogo" alt="" className="nav-logo" />
-      <img src="navProfile" className='nav-profile' alt="" />
+      <img src={navlogo} alt="" className="nav-logo" />
+      <img src= {navProfile} className='nav-profile' alt="" />
     </div>
   );
 };
