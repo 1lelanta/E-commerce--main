dff96d6b1a37fe63bcd98dc9a8df9af3f226d83c
1lelanta <kekikoki4@gmail.com>
2025-09-27 11:51:23 +0300

    add image handler using multer package to store image url in mongodb

--- admin/src/Components/AddProduct/AddProduct.jsx
+++ admin/src/Components/AddProduct/AddProduct.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react'
 import './AddProduct.css'
 import upload_area from '../../assets/upload_area.svg'
 
-
 const AddProduct = () => {
 
     const [image, setImage] = useState(false);
@@ -16,6 +15,28 @@ const AddProduct = () => {
 
     const changeHandler = (e)=>{
         setProductDetails({...productDetails,[e.target.name]:e.target.value})
+    }
+
+    const AddProduct = async ()=>{
+        console.log(productDetails)
+        let reponseData;
+        let product = productDetails;
+
+        let formData = new FormData();
+        formData.append('product', image);
+
+        await fetch('http://localhost:4000/upload',{
+            method:'POST',
+            headers:{
+                accept: 'application/json'
+            },
+            body:formData,
+        }).then((res)=>res.json()).then((data)=>{reponseData = data})
+
+        if(reponseData.success){
+            product.image = reponseData.image_url;
+            console.log(product)
+        }
     }
 
     const imageHandler =(e)=>{
@@ -33,12 +54,10 @@ const AddProduct = () => {
                 <div className="addproduct-itemfield">
                     <p>price</p>
                     <input value={productDetails.old_price} onChange={changeHandler} type="text" name='old_price' placeholder='type here' />
-
                 </div>
                 <div className="addproduct-itemfield">
                     <p>Offer Price</p>
                     <input value={productDetails.new_price} onChange={changeHandler} type="text" name='new_price' placeholder='type here' />
-
                 </div>
             </div>
             <div className="addproduct-itemfield">
@@ -47,16 +66,15 @@ const AddProduct = () => {
                     <option value="women">Women</option>
                     <option value="men">Men</option>
                     <option value="kid">Kid</option>
-
                 </select>
             </div>
             <div className="addproduct-itemfield">
                 <label htmlFor="file-input">
-                    <img onChange={imageHandler} src={image?URL.createObjectURL(image):upload_area}  className = 'addproduct-thumnail-img' alt="" />
+                    <img src={image ? URL.createObjectURL(image) : upload_area} className='addproduct-thumnail-img' alt="" />
                 </label>
-                <input type="file" name='image' id='file-input' hidden />
+                <input type="file" name='image' id='file-input' hidden onChange={imageHandler} />
             </div>
-            <button className='addproduct-btn'>Add</button>
+            <button onClick={AddProduct} className='addproduct-btn'>Add</button>
         </div>
     </div>
   )
