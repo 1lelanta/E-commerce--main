6ca611dea6a800596a1324b37444e25cd57d3469
1lelanta <kekikoki4@gmail.com>
2025-09-27 11:17:55 +0300

    add logic function to handle inputs

--- admin/src/Components/AddProduct/AddProduct.jsx
+++ admin/src/Components/AddProduct/AddProduct.jsx
@@ -6,6 +6,17 @@ import upload_area from '../../assets/upload_area.svg'
 const AddProduct = () => {
 
     const [image, setImage] = useState(false);
+    const [productDetails, setProductDetails] = useState({
+        name:"",
+        image:"",
+        category:"women",
+        new_price:"",
+        old_price:""
+    });
+
+    const changeHandler = (e)=>{
+        setProductDetails({...productDetails,[e.target.name]:e.target.value})
+    }
 
     const imageHandler =(e)=>{
         setImage(e.target.files[0])
@@ -15,24 +26,24 @@ const AddProduct = () => {
     <div className='add-product'>
         <div className="addproduct-itemfield">
             <p>Product title</p>
-            <input type="text" name='name' placeholder='Type here' />
+            <input value={productDetails.name} onChange={changeHandler} type="text" name='name' placeholder='Type here' />
         </div>
         <div>
             <div className="addproduct-price">
                 <div className="addproduct-itemfield">
                     <p>price</p>
-                    <input type="text" name='old_price' placeholder='type here' />
+                    <input value={productDetails.old_price} onChange={changeHandler} type="text" name='old_price' placeholder='type here' />
 
                 </div>
                 <div className="addproduct-itemfield">
                     <p>Offer Price</p>
-                    <input type="text" name='new_price' placeholder='type here' />
+                    <input value={productDetails.new_price} onChange={changeHandler} type="text" name='new_price' placeholder='type here' />
 
                 </div>
             </div>
             <div className="addproduct-itemfield">
                 <p>Product Category</p>
-                <select name="category" className='add-product-selector'>
+                <select value={productDetails.category} onChange={changeHandler} name="category" className='add-product-selector'>
                     <option value="women">Women</option>
                     <option value="men">Men</option>
                     <option value="kid">Kid</option>
