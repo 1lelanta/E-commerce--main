41ee893f1d85b2f131f4ae4069eeac458ddf9b30
1lelanta <kekikoki4@gmail.com>
2025-10-01 09:08:36 +0300

    feat(listProduct) add html structure for the list product page

--- admin/src/Components/ListProduct/ListProduct.jsx
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -4,7 +4,15 @@ import './ListProduct'
 export const ListProduct = () => {
   return (
     <div className='list-product'>
-
+      <h1>All Products List</h1>
+      <div className="listproduct-format-main">
+        <p>products</p>
+        <p>Title</p>
+        <p>Old Price</p>
+        <p>New Price</p>
+        <p>Category</p>
+        <p>Remove</p>
+      </div>
     </div>
   )
 }
