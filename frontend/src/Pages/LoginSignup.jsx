582d97b2a40b8ecc98b144319233ebc417059ee5
1lelanta <kekikoki4@gmail.com>
2025-10-02 20:22:46 +0300

    feat(frontend): add turnary to check if user already have an account

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './CSS/Loginsignup.css'
 
 const LoginSignup = () => {
+
+  const [state,setState] = useState("Login");
+
   return (
     <div className='loginsignup'>
       <div className="loginsignup-container">
-        <h1>Sign Up</h1>
+        <h1>{state}</h1>
         <div className="loginsignup-field">
-          <input type="text" placeholder='Your name' />
+          {state==="Sign Up"?<input type="text" placeholder='Your name' />:<></>}
           <input type="email" placeholder='Email Address' />
           <input type="password" placeholder='Password' />
         </div>
         <button>Continue</button>
         <p className="loginsignup-login">Already have an account? <span>Login</span></p>
+        <p className="loginsignup-login">Create an account? <span>Click here</span></p>
         <div className="loginsignup-agree">
           <input type="checkbox" name='' id='' />
           <p>By contiuning, i agree to the terms of use & privacy policy</p>
